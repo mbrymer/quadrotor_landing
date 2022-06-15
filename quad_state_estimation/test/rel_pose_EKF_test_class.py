Add normalization, fix Jacobians and initial state

quad_state_estimation/test/rel_pose_EKF_test_class.py
@@ -12,12 +12,14 @@ from cmath import pi
 import sys, copy, threading, math
 import rospy
 import numpy as np
+import tf
+
 from scipy.linalg import block_diag
-from quaternion_helper import quaternion_exp, skew_symm, quaternion_log
+from quaternion_helper import quaternion_exp, skew_symm, quaternion_log, quaternion_norm
 from tf.transformations import quaternion_matrix, quaternion_multiply, quaternion_about_axis, quaternion_conjugate, rotation_matrix
 
 # Import message types
-from geometry_msgs.msg import Point, PointStamped, Vector3, Quaternion, PoseWithCovariance, PoseWithCovarianceStamped, Pose
+from geometry_msgs.msg import Point, PointStamped, Vector3, Quaternion, PoseWithCovariance, PoseWithCovarianceStamped, Pose, PoseStamped
 from sensor_msgs.msg import Imu
 from apriltag_ros.msg import AprilTagDetection, AprilTagDetectionArray
 from std_msgs.msg import Float64, Header
@@ -30,11 +32,13 @@ class RelativePoseEKF(object):
         # Inputs
         self.IMU_msg = Imu()
         self.apriltag_msg = AprilTagDetectionArray()
-        self.measurement_ready = False
 
         # Outputs
         self.rel_pose_msg = PoseWithCovarianceStamped()
+        self.rel_pose_report_msg = PoseStamped()
         self.IMU_bias_msg = Imu()
+        self.tf_broadcast = tf.TransformBroadcaster()
+        self.tf_listener = tf.TransformListener()
 
         # Locks
         self.state_lock = threading.Lock()
@@ -46,14 +50,19 @@ class RelativePoseEKF(object):
         self.dT = 1/update_freq
         self.measurement_freq = measurement_freq
         self.upd_per_meas = math.ceil(update_freq/measurement_freq)
+        self.est_bias = True
 
         # State Storage
+        if self.est_bias:
+            self.num_states = 15
+        else:
+            self.num_states = 9
         self.r_nom = np.zeros((3,1))
         self.v_nom = np.zeros((3,1))
         self.q_nom = np.zeros(4)
         self.ab_nom = np.zeros((3,1))
         self.wb_nom = np.zeros((3,1))
-        self.cov_pert = np.zeros((15,15))
+        self.cov_pert = np.zeros((self.num_states,self.num_states))
 
         # Process and Measurement Noises
         self.r_cov_init = 0.1
@@ -62,15 +71,19 @@ class RelativePoseEKF(object):
         self.ab_cov_init = 0.5
         self.wb_cov_init = 0.1
 
-        self.cov_init = np.diag(np.hstack((self.r_cov_init*np.ones(3),self.v_cov_init*np.ones(3),
+        self.Q_a = 0.01*np.eye(3)
+        self.Q_w = 0.01*np.eye(3)
+        self.Q_ab = 5E-5*np.eye(3)
+        self.Q_wb = 5E-6*np.eye(3)
+
+        if self.est_bias:
+            self.cov_init = np.diag(np.hstack((self.r_cov_init*np.ones(3),self.v_cov_init*np.ones(3),
                         self.ang_cov_init*np.ones(3),self.ab_cov_init*np.ones(3),self.wb_cov_init*np.ones(3))))
-
-        self.Q_a = 10000*np.eye(3)
-        self.Q_w = 10000*np.eye(3)
-        self.Q_ab = 10000*np.eye(3)
-        self.Q_wb = 10000*np.eye(3)
-
-        self.Q = block_diag(self.Q_a,self.Q_w,self.Q_ab,self.Q_wb)
+            self.Q = block_diag(self.Q_a,self.Q_w,self.Q_ab,self.Q_wb)
+        else:
+            self.cov_init = np.diag(np.hstack((self.r_cov_init*np.ones(3),self.v_cov_init*np.ones(3),
+                        self.ang_cov_init*np.ones(3))))
+            self.Q = block_diag(self.Q_a,self.Q_w)
 
         self.R_r = 0.015*np.eye(3)
         self.R_ang = 0.05*np.eye(3)
@@ -79,16 +92,20 @@ class RelativePoseEKF(object):
 
         # Camera to Vehicle Transform
         self.r_v_cv = np.array([[0],[0],[-0.073]])
-        self.q_vc = np.array([0.70711,-0.70711,0,0])
+        self.q_vc = quaternion_norm(np.array([0.70711,-0.70711,0,0]))
         self.C_vc = quaternion_matrix(self.q_vc)[0:3,0:3]
 
         # Counters/flags
         self.state_initialized = False
+        self.measurement_ready = False
         self.upds_since_correction = 0
 
         # Tolerances and Constants
         self.small_ang_tol = 1E-10
-        self.g = np.array([[0],[0],[-9.81]])
+        self.g = np.array([[0],[0],[-9.8]])
+        self.pose_frame_name = "drone/rel_pose_est"
+        self.pose_report_frame_name = "drone/rel_pose_report"
+        self.pose_rel_frame_name = "target/tag_link"
 
     def filter_update(self):
         "Perform EKF update"
@@ -120,26 +137,32 @@ class RelativePoseEKF(object):
         # Propagate nominal state
         r_check = self.r_nom + self.dT*self.v_nom
         v_check = self.v_nom + self.dT*(np.dot(C_nom,a_nom) + self.g)
-        q_check = quaternion_multiply(self.q_nom,quaternion_exp(self.dT*w_nom.flatten()))
+        q_check = quaternion_norm(quaternion_multiply(self.q_nom,quaternion_exp(self.dT*w_nom.flatten())))
         ab_check = self.ab_nom
         wb_check = self.wb_nom
 
         # Calculate Jacobians
-        F_km1 = np.eye(15)
+        F_km1 = np.eye(self.num_states)
         F_km1[0:3,3:6] = self.dT*np.eye(3)
-        F_km1[3:6,6:9] = -self.dT*np.dot(C_nom,skew_symm(a_nom))
-        F_km1[3:6,9:12] = -self.dT*C_nom
-
+        F_km1[3:6,6:9] = -self.dT*np.dot(C_nom,skew_symm(a_nom.flatten()))
+        
         w_int = self.dT*w_nom
         w_int_angle = np.linalg.norm(w_int)
         if w_int_angle<self.small_ang_tol:
-            F_km1[6:9,6:9] = np.eye(3)
+            # Avoid zero division, approximate with first order Taylor series
+            F_km1[6:9,6:9] = np.eye(3)-skew_symm(w_int.flatten())
         else:
+            # Large angle, use full Rodriguez formula
             w_int_axis = w_int/w_int_angle
             F_km1[6:9,6:9] = rotation_matrix(-w_int_angle,w_int_axis)[0:3,0:3]
-        F_km1[6:9,12:15] = -self.dT*np.eye(3)
         
-        W_km1 = block_diag(np.vstack((np.zeros((3,3)),-C_nom)),np.eye(9))
+        if self.est_bias:
+            F_km1[3:6,9:12] = -self.dT*C_nom
+            F_km1[6:9,12:15] = -self.dT*np.eye(3)
+        
+            W_km1 = block_diag(np.vstack((np.zeros((3,3)),-C_nom)),np.eye(9))
+        else:
+            W_km1 = block_diag(np.vstack((np.zeros((3,3)),-C_nom)),np.eye(3))
 
         # Propagate covariance
         P_check = np.linalg.multi_dot((F_km1,self.cov_pert,F_km1.T)) + np.linalg.multi_dot((W_km1,self.Q,W_km1.T))
@@ -157,7 +180,7 @@ class RelativePoseEKF(object):
             # Convert AprilTag readings to vehicle state coordinates
             C_check = quaternion_matrix(q_check)[0:3,0:3]
             r_t_vt_obs = -np.linalg.multi_dot((C_check,self.C_vc,r_c_tc))-np.dot(C_check,self.r_v_cv)
-            q_tv_obs = quaternion_conjugate(quaternion_multiply(self.q_vc,q_ct))
+            q_tv_obs = quaternion_norm(quaternion_conjugate(quaternion_multiply(self.q_vc,q_ct)))
 
             # Calculate observed perturbations in measurements
             delta_r_obs = r_t_vt_obs - r_check
@@ -165,9 +188,10 @@ class RelativePoseEKF(object):
             delta_theta_obs = quaternion_log(delta_q_obs).reshape((3,1))
 
             # Calculate Jacobians
-            G_k = np.zeros((6,15))
+            G_k = np.zeros((6,self.num_states))
+
             G_k[0:3,0:3] = np.eye(3)
-            G_k[0:3,6:9] = -np.dot(C_check,skew_symm(np.dot(C_check.T,r_check)))
+            G_k[0:3,6:9] = np.dot(C_check,skew_symm(np.dot(C_check.T,r_check)))
             G_k[3:6,6:9] = np.eye(3)
 
             N_k = block_diag(-np.dot(C_check,self.C_vc),self.C_vc)
@@ -178,15 +202,17 @@ class RelativePoseEKF(object):
             Cov_meas_inv = np.linalg.inv(np.linalg.multi_dot((G_k,P_check,G_k.T))+R_k)
             K_k = np.linalg.multi_dot((P_check,G_k.T,Cov_meas_inv))
 
-            P_hat = np.dot(np.eye(15)-np.dot(K_k,G_k),P_check)
+            P_hat = np.dot(np.eye(self.num_states)-np.dot(K_k,G_k),P_check)
             delta_x_hat = np.dot(K_k,np.vstack((delta_r_obs,delta_theta_obs)))
 
             # Inject correction update, store and reset error state
             self.r_nom = r_check + delta_x_hat[0:3,0:1]
             self.v_nom = v_check + delta_x_hat[3:6,0:1]
-            self.q_nom = quaternion_multiply(q_check,quaternion_exp(delta_x_hat[6:9,0:1].flatten()))
-            self.ab_nom = ab_check + delta_x_hat[9:12,0:1]
-            self.wb_nom = wb_check + delta_x_hat[12:15,0:1]
+            self.q_nom = quaternion_norm(quaternion_multiply(q_check,quaternion_exp(delta_x_hat[6:9].flatten())))
+
+            if self.est_bias:
+                self.ab_nom = ab_check + delta_x_hat[9:12,0:1]
+                self.wb_nom = wb_check + delta_x_hat[12:15,0:1]
             
             self.cov_pert = P_hat
             self.upds_since_correction = 0
@@ -202,8 +228,9 @@ class RelativePoseEKF(object):
 
             self.upds_since_correction += 1
 
-        # Pack estimate up into messages
-        curr_header = Header(stamp=rospy.get_rostime())
+        # Pack estimate up into messages and publish transform
+        curr_time = rospy.get_rostime()
+        curr_header = Header(stamp=curr_time,frame_id=self.pose_frame_name)
         pose_cov = np.vstack((np.hstack((self.cov_pert[0:3,0:3],self.cov_pert[0:3,6:9])),
                             np.hstack((self.cov_pert[6:9,0:3],self.cov_pert[6:9,6:9]))))
         new_rel_pose = PoseWithCovariance(pose = Pose(position = Point(x=self.r_nom[0],y=self.r_nom[1],z=self.r_nom[2]),
@@ -213,18 +240,35 @@ class RelativePoseEKF(object):
         self.IMU_bias_msg = Imu(header=curr_header,
                                 linear_acceleration = Vector3(x=self.ab_nom[0],y=self.ab_nom[1],z=self.ab_nom[2]),
                                 angular_velocity = Vector3(x=self.wb_nom[0],y=self.wb_nom[1],z=self.wb_nom[2]))
+        
+        self.tf_broadcast.sendTransform((tuple(self.r_nom)), (tuple(self.q_nom)), curr_time,self.pose_frame_name,self.pose_rel_frame_name)
 
-    def initialize_state(self):
+        if perform_correction:
+            self.rel_pose_report_msg = PoseStamped(header=curr_header,
+            pose = Pose(position = Point(x=r_t_vt_obs[0],y=r_t_vt_obs[1],z=r_t_vt_obs[2]),
+                            orientation = Quaternion(x=q_tv_obs[0],y=q_tv_obs[1],z=q_tv_obs[2],w=q_tv_obs[3])))
+            self.tf_broadcast.sendTransform((tuple(r_t_vt_obs)), (tuple(q_tv_obs)), curr_time,self.pose_report_frame_name,self.pose_rel_frame_name)
+
+    def initialize_state(self,reinit_bias):
         "Initialize state to last received AprilTag relative pose detection"
         self.state_lock.acquire()
-        self.r_nom[0] = self.apriltag_msg.detections[0].pose.pose.pose.position.x
-        self.r_nom[1] = self.apriltag_msg.detections[0].pose.pose.pose.position.y
-        self.r_nom[2] = self.apriltag_msg.detections[0].pose.pose.pose.position.z
+        
+        # Assume identity rotation to start. Will need to initialize this based on GPS in real implementation for stability most likely
+        r_c_tc = np.array([[self.apriltag_msg.detections[0].pose.pose.pose.position.x],
+                                [self.apriltag_msg.detections[0].pose.pose.pose.position.y],
+                                [self.apriltag_msg.detections[0].pose.pose.pose.position.z]])
+        q_ct = np.array([self.apriltag_msg.detections[0].pose.pose.pose.orientation.x,
+                                self.apriltag_msg.detections[0].pose.pose.pose.orientation.y,
+                                self.apriltag_msg.detections[0].pose.pose.pose.orientation.z,
+                                self.apriltag_msg.detections[0].pose.pose.pose.orientation.w])
+        
+        self.r_nom = -np.dot(self.C_vc,r_c_tc)-self.r_v_cv
         self.v_nom = np.zeros((3,1))
-        self.q_nom[0] = self.apriltag_msg.detections[0].pose.pose.pose.orientation.x
-        self.q_nom[1] = self.apriltag_msg.detections[0].pose.pose.pose.orientation.y
-        self.q_nom[2] = self.apriltag_msg.detections[0].pose.pose.pose.orientation.z
-        self.q_nom[3] = self.apriltag_msg.detections[0].pose.pose.pose.orientation.w
+        self.q_nom = quaternion_norm(quaternion_conjugate(quaternion_multiply(self.q_vc,q_ct)))
+
+        if reinit_bias:
+            self.ab_nom = 0
+            self.wb_nom = 0
 
         self.cov_pert = self.cov_init
 

quad_state_estimation/test/rel_pose_EKF_test_node.py
@@ -15,7 +15,7 @@ from tf.transformations import quaternion_from_euler, euler_from_quaternion, qua
 from rel_pose_EKF_test_class import RelativePoseEKF
 
 # Import message types
-from geometry_msgs.msg import Point, Vector3, Quaternion, PoseWithCovarianceStamped
+from geometry_msgs.msg import Point, Vector3, Quaternion, PoseStamped,PoseWithCovarianceStamped
 from sensor_msgs.msg import Imu
 from apriltag_ros.msg import AprilTagDetection, AprilTagDetectionArray
 from std_msgs.msg import Float64
@@ -26,13 +26,13 @@ class RelativePoseEKFNode(object):
         
         # Rates:
         self.update_freq = 100 # Hz
-        self.measurement_freq = 10 # Hz
+        self.measurement_freq = 100 # Hz
 
         # Objects:
         self.rel_pose_ekf = RelativePoseEKF(self.update_freq,self.measurement_freq)
 
         # Subscribers:
-        self.IMU_topic = '/drone/ground_truth/imu'
+        self.IMU_topic = '/drone/imu'
         self.apriltag_topic = 'tag_detections'
 
         self.IMU_sub = rospy.Subscriber(self.IMU_topic,Imu,callback=self.IMU_sub_callback)
@@ -40,8 +40,10 @@ class RelativePoseEKFNode(object):
 
         # Publishers:
         self.rel_pose_topic = '/state_estimation/rel_pose_state'
+        self.rel_pose_report_topic = 'state_estimation/rel_pose_reported'
         self.IMU_bias_topic = '/state_estimation/IMU_bias'
         self.rel_pose_pub = rospy.Publisher(self.rel_pose_topic,PoseWithCovarianceStamped,queue_size=1)
+        self.rel_pose_report_pub = rospy.Publisher(self.rel_pose_report_topic,PoseStamped,queue_size=1)
         self.IMU_bias_pub = rospy.Publisher(self.IMU_bias_topic,Imu,queue_size=1)
 
         # Timers:
@@ -61,7 +63,7 @@ class RelativePoseEKFNode(object):
             self.rel_pose_ekf.measurement_ready = True
         
             if not self.rel_pose_ekf.state_initialized:
-                self.rel_pose_ekf.initialize_state()
+                self.rel_pose_ekf.initialize_state(False)
             
             self.rel_pose_ekf.apriltag_lock.release()
 
@@ -72,6 +74,7 @@ class RelativePoseEKFNode(object):
         # Publish state estimate
         self.rel_pose_pub.publish(self.rel_pose_ekf.rel_pose_msg)
         self.IMU_bias_pub.publish(self.rel_pose_ekf.IMU_bias_msg)
+        self.rel_pose_report_pub.publish(self.rel_pose_ekf.rel_pose_report_msg)
 
 if __name__ == '__main__':
     try: